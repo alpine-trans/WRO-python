7f63964363dec83dc6ea28045dc91b4ebcbe3435
alpine-trans <127592999+alpine-trans@users.noreply.github.com>
2025-02-08 17:14:03 +0900

    Create Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,9 @@
+# 辞書
+# 配列の亜種みたいなやつ
+# 辞書名 = {キー1：値1, キー2：値2, ...}
+# キー(key)と値を対応させる。キーには変更不可能なデータを入れる(文字列，数値，タプル 等)。
+family_ages = {'son' : 14, 'mother' : 43, 'father' : 45}     # 例 家族の年齢についての辞書
+print(family_ages)
+# ↳ {'son' : 14, 'mother' : 43, 'father' : 45}
+print(family_ages['son'])
+# ↳ 14
